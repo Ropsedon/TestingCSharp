73425eee3c92e55286b592616e8d1a76e80d5c7d
Ropsedon <alexsandr.glukhov@gmail.com>
2019-07-30 22:46:31 +0300

    Add switch constraction

---
+++ YouTube/YouTube/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace YouTube
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Whrite your number");
+            string text = Console.ReadLine();
+            int x = Convert.ToInt32(text);
+
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("first one");
+                    break;
+                case 2:
+                    Console.WriteLine("second one");
+                    break;
+                case 3:
+                    Console.WriteLine("third one");
+                    break;
+                default:
+                    Console.WriteLine("fin");
+                    break;
+            }
+
+            Console.WriteLine("all");
+            Console.ReadKey();
+        }
+    }
+}
